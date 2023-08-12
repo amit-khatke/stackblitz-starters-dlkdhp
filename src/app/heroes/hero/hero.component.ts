7773bc2294055f7e39a41fb566387abd161d2617
amit-khatke <141109542+amit-khatke@users.noreply.github.com>
2023-08-12 14:05:44 +0530

    3 files modified, 3 added

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Hero } from '../../hero';
+
+@Component({
+  standalone:true,
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css'],
+})
+export class HeroComponent implements OnInit {
+  @Input() hero?:Hero;
+  constructor() { }
+
+  ngOnInit() {
+    // console.log(this.hero);
+  }
+
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,22 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroComponent } from './hero/hero.component';
 
 @Component({
   standalone:true,
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
+  imports:[HeroComponent,CommonModule],
 })
 export class HeroesComponent implements OnInit {
 
-  heroes:Hero[]=HEROES;
+  heroes:Hero[]=[];
   
   constructor() {
-    console.log(this.heroes);
+    
    }
 
   ngOnInit() {
+    this.heroes=HEROES;
+    // console.log(this.heroes);
   }
 
 }
